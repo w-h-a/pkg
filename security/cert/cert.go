7ad81c8111b082c9d82af68473fdce0a3a3ec827
w-h-a <wesley.herb.anderson@gmail.com>
2024-08-03 13:46:29 -0700

    refactor: param name

--- security/cert/cert.go
+++ security/cert/cert.go
@@ -4,6 +4,6 @@ import "net"
 
 type CertProvider interface {
 	Options() CertOptions
-	Listener(domains ...string) (net.Listener, error)
+	Listener(hosts ...string) (net.Listener, error)
 	String() string
 }

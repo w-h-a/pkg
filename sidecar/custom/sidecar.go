ba2041f2a41961c36c91e4e089b37f9779b5518a
w-h-a <wesley.herb.anderson@gmail.com>
2024-08-25 11:28:03 -0700

    chore: debug

--- sidecar/custom/sidecar.go
+++ sidecar/custom/sidecar.go
@@ -237,7 +237,7 @@ func (s *customSidecar) sendEventViaHttp(namespace, name, port, endpoint, baseUr
 		return nil, err
 	}
 
-	log.Infof("RESPONSE FROM APP %+v", rsp)
+	log.Infof("RESPONSE FROM APP %+v", rsp.State)
 
 	return rsp, nil
 }

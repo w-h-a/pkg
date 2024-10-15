9fb0c6144067b86e9e1c3072e6582666c72b7b07
w-h-a <wesley.herb.anderson@gmail.com>
2024-10-14 21:14:03 -0700

    refactor: have grpc use bytes

--- sidecar/utils.go
+++ sidecar/utils.go
@@ -4,20 +4,13 @@ import (
 	"encoding/json"
 
 	pb "github.com/w-h-a/pkg/proto/sidecar"
-	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func SerializeEvent(event *Event) (*pb.Event, error) {
-	kvs := []*pb.KeyVal{}
-
-	for k, v := range event.Payload {
-		bytes, _ := json.Marshal(v)
-		kv := &pb.KeyVal{Key: k, Value: &anypb.Any{Value: bytes}}
-		kvs = append(kvs, kv)
-	}
+	bs, _ := json.Marshal(event.Payload)
 
 	return &pb.Event{
 		EventName: event.EventName,
-		Payload:   kvs,
+		Payload:   bs,
 	}, nil
 }
